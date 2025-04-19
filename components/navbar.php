65433026708441c4bd0b68fe7ae4ae5d7269d53b
Leandra <andrawisnu60@gmail.com>
2025-04-19 11:55:25 +0700

    Navbar Done

---
+++ components/navbar.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div class="header-strip small-text background-color-sinbarBlue color-sinbarWhite center">
+        <p>X RPL B - SMK NEGERI 4 MALANG</p>
+    </div>
+    <div class="navbar background-color-sinbarWhite">
+        <div class="container-navbar-logo">
+            <div class="navbar-logo color-sinbarBlue">
+                <img src="./assets/icons/logo.svg" height="100%">
+                <p class="title">SinBar</p>
+            </div>
+        </div>
+        <div class="navbar-menus">
+            <a class="navbar-menus-menu regular-text color-sinbarBlack">HOME</a>
+            <a class="navbar-menus-menu regular-text color-sinbarBlack">MATERI</a>
+            <a class="navbar-menus-menu regular-text color-sinbarBlack">PERINGKAT</a>
+            <a class="navbar-menus-menu regular-text color-sinbarBlack">KONTAK</a>
+        </div>
+        <?php
+        if (isset($_SESSION['log'])) { ?>
+
+        <?php }
+        ?>
+        <div class="container-navbar-account">
+            <div class="navbar-account-logged">
+                <a class="navbar-account-logged-menu regular-text background-color-sinbarBlue color-sinbarWhite">AKUN</a>
+                <a class="navbar-account-logged-logout regular-text color-sinbarWhite">
+                    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M2 18C1.45 18 0.979333 17.8043 0.588 17.413C0.196667 17.0217 0.000666667 16.5507 0 16V2C0 1.45 0.196 0.979333 0.588 0.588C0.98 0.196667 1.45067 0.000666667 2 0H9V2H2V16H9V18H2ZM13 14L11.625 12.55L14.175 10H6V8H14.175L11.625 5.45L13 4L18 9L13 14Z" />
+                    </svg>
+                </a>
+            </div>
+        </div>
+    </div>
+    <hr style="width: 100%;" class="color-sinbarDarkGrey">
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Beranda - SinBarTIK</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <?php include './components/navbar.php' ?>
+</body>
+
+</html>
